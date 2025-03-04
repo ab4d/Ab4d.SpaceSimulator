ea08db00b7b46f9c1d90cf153e3d1b4e9910556f
Andrej Benedik <abenedik@ab4d.com>
2025-03-04 12:36:56 +0100

    Uncommented calling SetLicense

--- Ab4d.SpaceSimulator.Desktop/Program.cs
+++ Ab4d.SpaceSimulator.Desktop/Program.cs
@@ -21,10 +21,10 @@ namespace Ab4d.SpaceSimulator.Desktop
             //
             // This is just temporal trial license and will be replaced by a free open source license:
             // Ab4d.SharpEngine Trial License can be used for testing the Ab4d.SharpEngine and is valid until April 15, 2025.
-            //Ab4d.SharpEngine.Licensing.SetLicense(licenseOwner: "AB4D",
-            //                                      licenseType: "TrialLicense",
-            //                                      platforms: "All",
-            //                                      license: "4045-4200-4577-BCD0-4186-154D-0519-2487-6A96-1677-6027-87D1-C2DF-D1D5-3064-F1B1-34EA");
+            Ab4d.SharpEngine.Licensing.SetLicense(licenseOwner: "AB4D",
+                                                  licenseType: "TrialLicense",
+                                                  platforms: "All",
+                                                  license: "4045-4200-4577-BCD0-4186-154D-0519-2487-6A96-1677-6027-87D1-C2DF-D1D5-3064-F1B1-34EA");
 
             return AppBuilder.Configure<App>()
                              .UsePlatformDetect()
